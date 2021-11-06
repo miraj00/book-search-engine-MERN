85f26eaa62425c088605a39cc4b914ef4f341033
miraj <miraj00@gmail.com>
2021-11-06 14:33:06 -0400

    Back-End specifications on typedefs and resolvers added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,13 +2,42 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql`  type Query {
-    helloWorld: String
-  }`;
+const typeDefs = gql`  
 
+        type Query {
+            me: User
+        }
 
+        type Mutation {
+            login(email: String!, password: String!): Auth
+            addUser(username: String!, email: String!, password: String!): Auth
+            saveBook(input: bookInput): User
+            removeBook(bookId: String!): User
+        }
+        
+        type User {
+            _id: ID
+            username: String
+            email: String
+            bookCount: Int
+            savedBooks: [Book]
+        }
 
+        type Book {
+            bookId: String
+            authors: [String]
+            description: String
+            title: String
+            image: String
+            link: String
+        }
+     
+        type Auth {
+            token: ID
+            user: User
+        }
 
+`;
 
 
 // export the typeDefs
