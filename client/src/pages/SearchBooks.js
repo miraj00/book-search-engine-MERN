24e2a7df1d1a1ba35296df3b04efbc5e735bcba3
miraj <miraj00@gmail.com>
2021-11-07 00:54:41 -0400

    heroku deployment error debug in progress

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,10 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
+// import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import {SAVE_BOOK} from '../utils/mutations';
 import {useMutation} from '@apollo/client';
+
+
+const searchGoogleBooks = (query) => {
+  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
 
 
   const SearchBooks = () => {
